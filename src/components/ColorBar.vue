fb74acdbf0d1e2ecfb3e576d5b8aacc843a8b404
FrozenMango747 <realsaumya@gmail.com>
2020-09-06 15:29:10 -0700

    Data

---
+++ src/components/ColorBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ColorBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,21 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+function makeArray(x: number,y: number,d: any) : Array<number> {
+    return (new Array(y)).fill((new Array(x)).fill(d))
+}
+
+
+interface Cell {
+  people: Array<any>,
+  type: string
+}
+
 export default new Vuex.Store({
   state: {
+    grid: makeArray(100,60,1),
+    focusedColor:"road",
+    availableColors: ["road","grass"]
   },
   mutations: {
   },
